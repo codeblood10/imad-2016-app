ee6572154870fae48d52b02c013b193fa2b2fa68
codeblood10 <ankitshr8@gmail.com>
2016-10-10 01:03:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/article-one',funciton(req, res){ 
+res.send('Article one requested and will be serverd here');
+}); 
+app.get('/article-two',funciton(req,res){ 
+res.send('Article two requested and will be serverd here');
+});
+app.get('/article-three',funciton(req,res){ 
+res.send('Article three requested and will be serverd here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
