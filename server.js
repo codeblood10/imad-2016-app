7f2e9688802acb6884d384ad7685bdeb20fc3e0b
codeblood10 <ankitshr8@gmail.com>
2016-11-24 03:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/test-db',function(req,res){
              res.status(500).send(err.toString());
          } else 
          { 
-             res.send(JSON.stringfy(result.rows)); 
+             res.send(JSON.stringify(result.rows)); 
          }
      });
 }) ;
